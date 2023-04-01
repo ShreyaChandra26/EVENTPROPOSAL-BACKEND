00a078c09b246de030362044f485dc1fa44ee131
ShreyaChandra26 <124866282+ShreyaChandra26@users.noreply.github.com>
2023-04-01 22:54:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req,res,next)=>[
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000"),
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:8080"),
     res.header(
         "Access-Control-Allow-Headers",
         "Origin,X-Requested-With,Content-Type,Accept"
