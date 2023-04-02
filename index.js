2c61d9c629a2cc15a851e727d26e617e3a6b9c8c
ShreyaChandra26 <124866282+ShreyaChandra26@users.noreply.github.com>
2023-04-02 08:44:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express=require('express')
 const app=express()
-// var cors = require('cors');
+const cors = require('cors');
 const PORT=8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req,res,next)=>[
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:8080"),
+
+app.use(cors())
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+        );
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With,Content-Type,Accept"
-    ),
-    next(),
-])
+        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+        );
+    next();
+});
+
+
 
 require("./connection/connection")
 require("./schemas/user")
