230e22e629fb37836111b7b32c7efb76960ba373
ShreyaChandra26 <124866282+ShreyaChandra26@users.noreply.github.com>
2023-04-02 08:54:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express=require('express')
 const app=express()
-const cors = require('cors');
+// const cors = require('cors');
 const PORT=8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use(cors())
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
     res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
         "Access-Control-Allow-Methods",
-        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-        );
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-        );
-    next();
-});
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 
 
 
